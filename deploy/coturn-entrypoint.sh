e102dadd5fc08dcf13519e8ff002dd696fd288d1
Anatoly Antonenko <ya.tolik-a@yandex.ru>
2026-02-06 16:49:28 +0300

    fix

--- deploy/coturn-entrypoint.sh
+++ deploy/coturn-entrypoint.sh
@@ -1,1 +1,6 @@
-exec turnserver -c /etc/coturn/turnserver.conf --static-auth-secret="${TURN_SECRET}"
+#!/bin/sh
+if [ -n "$TURN_USE_AUTH" ] && [ -n "$TURN_SECRET" ]; then
+  exec turnserver -c /etc/coturn/turnserver.conf --static-auth-secret="${TURN_SECRET}"
+else
+  exec turnserver -c /etc/coturn/turnserver.conf
+fi
